b4daaa96021f3afebd66137d2b5c653a7d5cf383
Kavindu Amarasinghe <amarasinghekavindu@yahoo.com>
2017-11-17 11:08:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string){
 
 function sayHiToGrandma(string){
   if(string !== string.toUpperCase()){
-    return('"I can't hear you!"');
+    return("I can\'t hear you!");
   } else if (string !== string.toLowerCase()){
     return("YES INDEED!");
   } else if (string === "I love you, Grandma."){
